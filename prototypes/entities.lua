918601b03c6fa530fdc8824904189e2d7c4a9e49
Jackalope-Gaming <jwhurst13@yahoo.com>
2016-04-08 14:33:28 -0600

    Update entities.lua

--- prototypes/entities.lua
+++ prototypes/entities.lua
@@ -2,10 +2,10 @@ supported_wagons = {
     -- base game
     "cargo-wagon",
 
-    -- rail tanker
+    -- Rail Tanker
     "rail-tanker",
 
-    -- bobs mods
+    -- Bob's mods
     "cargo-wagon-2",
     "cargo-wagon-3",
     "armoured-cargo-wagon",
@@ -18,16 +18,16 @@ supported_locos = {
     -- FARL
     "farl",
 
-    -- bob's mods
+    -- Bob's mods
     "diesel-locomotive-2",
     "diesel-locomotive-3",
     "armoured-diesel-locomotive",
 
-    -- more locomotives
+    -- More Locomotives
     "cargo-locomotive",
     "hs-locomotive",
 
-    -- color-coding colored locos
+    -- Color-coding colored locos
     "diesel-locomotive-red",
     "diesel-locomotive-orange",
     "diesel-locomotive-yellow",
@@ -44,10 +44,10 @@ supported_locos = {
 -- reconfigure all supported cargo wagons for 8L size
 for i,name in ipairs(supported_wagons) do
     if  data.raw["cargo-wagon"][name] then
-        data.raw["cargo-wagon"][name].collision_box = {{-0.6, -2.6}, {0.6, 2.6}}
+        data.raw["cargo-wagon"][name].collision_box = {{-0.6, -2.7}, {0.6, 2.7}}
         data.raw["cargo-wagon"][name].selection_box = {{-1, -4.5}, {1, 3}}
-        data.raw["cargo-wagon"][name].connection_distance = 3.4
-        data.raw["cargo-wagon"][name].joint_distance = 4.6
+        data.raw["cargo-wagon"][name].connection_distance = 3
+        data.raw["cargo-wagon"][name].joint_distance = 5
         data.raw["cargo-wagon"][name].pictures.scale = 1.095
         data.raw["cargo-wagon"][name].pictures.shift = {0.735, -0.55}
         if name == "rail-tanker" then
@@ -61,10 +61,10 @@ end
 -- reconfigure all supported locos for 8L size
 for i,name in ipairs(supported_locos) do
     if  data.raw["locomotive"][name] then
-        data.raw["locomotive"][name].collision_box = {{-0.6, -2.6}, {0.6, 2.6}}
+        data.raw["locomotive"][name].collision_box = {{-0.6, -2.7}, {0.6, 2.7}}
         data.raw["locomotive"][name].selection_box = {{-1, -4.5}, {1, 3}}
-        data.raw["locomotive"][name].connection_distance = 3.4
-        data.raw["locomotive"][name].joint_distance = 4.6
+        data.raw["locomotive"][name].connection_distance = 3
+        data.raw["locomotive"][name].joint_distance = 5
         data.raw["locomotive"][name].pictures.shift = {1.0, -0.51}
     end
 end
