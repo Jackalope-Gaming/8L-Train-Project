1dd87ff01e2ce1eda26a323ab1e3bcce9b645b12
Jackalope-Gaming <jwhurst13@yahoo.com>
2016-04-12 15:03:26 -0600

    Added support for Factorio Extended

--- prototypes/entities.lua
+++ prototypes/entities.lua
@@ -1,29 +1,33 @@
 supported_wagons = {
-    -- base game
+    -- base game wagon
     "cargo-wagon",
 
-    -- Rail Tanker
+    -- Rail Tanker's wagon
     "rail-tanker",
 
-    -- Bob's mods
+    -- Bob's mods wagons
     "cargo-wagon-2",
     "cargo-wagon-3",
     "armoured-cargo-wagon",
+	
+	--Factorio Extended wagons
+	"cargo-wagon-mk2",
+	"cargo-wagon-mk3",
 }
 
 supported_locos = {
-    -- base game
+    -- base game loco
     "diesel-locomotive",
 
-    -- FARL
+    -- FARL's loco
     "farl",
 
-    -- Bob's mods
+    -- Bob's mods locos
     "diesel-locomotive-2",
     "diesel-locomotive-3",
     "armoured-diesel-locomotive",
 
-    -- More Locomotives
+    -- More Locomotives' locos
     "cargo-locomotive",
     "hs-locomotive",
 
@@ -38,13 +42,17 @@ supported_locos = {
     "diesel-locomotive-magenta",
     "diesel-locomotive-white",
     "diesel-locomotive-black",
+	
+	-- Factorio Extended locos
+	"diesel-locomotive-mk2",
+	"diesel-locomotive-mk3",
 }
 
 
 -- reconfigure all supported cargo wagons for 8L size
 for i,name in ipairs(supported_wagons) do
     if  data.raw["cargo-wagon"][name] then
-        data.raw["cargo-wagon"][name].collision_box = {{-0.6, -2.75}, {0.6, 2.75}}
+        data.raw["cargo-wagon"][name].collision_box = {{-0.6, -2.7}, {0.6, 2.7}}
         data.raw["cargo-wagon"][name].selection_box = {{-1, -4.5}, {1, 3}}
         data.raw["cargo-wagon"][name].connection_distance = 3
         data.raw["cargo-wagon"][name].joint_distance = 5
